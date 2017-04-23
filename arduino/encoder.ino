cd5f3e1fbbf5360e1611e8f0db11cab90de0ce8b
ridhwanluthra <ridhwancode@gmail.com>
2017-04-23 21:51:08 +0530

    trying to find the right registers for interrupts in arduino

--- arduino/encoder.ino
+++ arduino/encoder.ino
@@ -6,24 +6,33 @@
 volatile int left_encoder = 0;
 volatile int right_encoder = 0;
 
-int sensePin = 2;
+//int sensePin = 3;
 // Install the interrupt routine.
-ISR(INT0_vect) {
+ISR(INT4_vect) {
   // check the value again - since it takes some time to
   // activate the interrupt routine, we get a clear signal.
   left_encoder++;
 }
 
+ISR(INT5_vect) {
+  // check the value again - since it takes some time to
+  // activate the interrupt routine, we get a clear signal.
+  right_encoder++;
+}
+
 
 void setup() {
   Serial.begin(9600);
-  pinMode(sensePin, INPUT);
-  EIMSK |= ( 1 << INT0);
-  // Signal change triggers interrupt
-  MCUCR |= ( 1 << ISC00);
-  MCUCR |= ( 0 << ISC01);
+//  pinMode(sensePin, INPUT);
+  // Set INT4 as falling edge triggered
+  EICRB |= (1 << ISC40);
+  EICRB |= (1 << ISC50);
+   
+  // Enable INT4 interrupt
+  EIMSK |= (1 << INT4);
+  EIMSK |= (1 << INT5);
 }
 
 void loop() {
-  Serial.println(left_encoder);
+  Serial.println(right_encoder);
 }
