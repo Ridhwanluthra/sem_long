7967e0486637ba6d161cd7a6623693b76a7da500
ridhwanluthra <ridhwancode@gmail.com>
2017-05-06 14:42:10 +0530

    added git install script

---
+++ ros_kinect_install_script.sh
@@ -0,0 +1,28 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt-get install ros-kinetic-openni-launch
+
+cd
+git clone https://github.com/avin2/SensorKinect
+cd SensorKinect/Bin
+tar xjf SensorKinect093-Bin-Linux-x64-v5.1.2.1.tar.bz2
+cd Sensor-Bin-Linux-x64-v5.1.2.1
+sudo ./install.sh
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws
+
+catkin_make
+
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt-get install ros-kinetic-rtabmap-ros
